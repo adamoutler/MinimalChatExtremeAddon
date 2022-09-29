f8571bfee4a782d619cc040eb6a7ce72fb5a4bb1
Adam Outler <adamoutler@gmail.com>
2022-09-28 23:48:54 -0400

    Update run.sh

--- minimalChat/run.sh
+++ minimalChat/run.sh
@@ -6,7 +6,7 @@ mkdir -p /data/rooms
 ln -s /data/rooms /var/www/MinimalChatExtreme/rooms;
 chmod 777 /var/www/MinimalChatExtreme/rooms
 
-php-fpm7;
+php-fpm8;
 nginx -g 'daemon off;error_log /dev/stdout debug;'
 
 until false; do
